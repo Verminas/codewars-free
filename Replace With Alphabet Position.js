cb1cc0e349923f90382b07a18f0f9e09f6099d3e
Verminas <34026266+Verminas@users.noreply.github.com>
2024-02-10 02:24:20 +0300

    Update Replace With Alphabet Position.js

--- Replace With Alphabet Position.js
+++ Replace With Alphabet Position.js
@@ -30,3 +30,15 @@ function alphabetPosition(text) {
   });
   return result.filter(Boolean).join(' ');
 }
+
+//clever solution
+
+function alphabetPosition(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++){
+    var code = text.toUpperCase().charCodeAt(i)
+    if (code > 64 && code < 91) result += (code - 64) + " ";
+  }
+
+  return result.slice(0, result.length-1);
+}
