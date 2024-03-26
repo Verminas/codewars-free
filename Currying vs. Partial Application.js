5b46d5b6b6949ed8aee270f499bd81961487c9de
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-26 20:25:51 +0300

    Update Currying vs. Partial Application.js

--- Currying vs. Partial Application.js
+++ Currying vs. Partial Application.js
@@ -1,2 +1,8 @@
 // https://www.codewars.com/kata/53cf7e37e9876c35a60002c9/train/javascript
 
+function curryPartial(fn, ...args) {
+  if (args.length >= fn.length) {
+    return fn(...args);
+  }
+  return (...params) => curryPartial(fn, ...args, ...params);
+}
