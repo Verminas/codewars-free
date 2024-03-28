b64afd0f6eaac4accb539e0d30e3c240b54bf193
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-28 22:51:45 +0300

    Update The Millionth Fibonacci Kata.js

--- The Millionth Fibonacci Kata.js
+++ The Millionth Fibonacci Kata.js
@@ -69,3 +69,15 @@ function fib(n) {
      return sign * poweredMatrix[0][0]; 
     }
   }
+
+// short variant
+
+const fib = n =>
+  (fn => n < 0 && !(n % 2) ? -fn(Math.abs(n))[0] : fn(Math.abs(n))[0])
+  (function fn(n) {
+    if (!n) return [0n, 1n];
+    const [_n, _n1] = fn(n >> 1);
+    const _2n = _n * (2n * _n1 - _n);
+    const _2n1 = _n ** 2n + _n1 ** 2n;
+    return n % 2 ? [_2n1, _2n + _2n1] : [_2n, _2n1];
+  });
