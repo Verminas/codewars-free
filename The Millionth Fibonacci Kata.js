0dfbd677133e12f112c2d43bded000e54b7164f0
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-28 22:30:57 +0300

    Update The Millionth Fibonacci Kata.js

--- The Millionth Fibonacci Kata.js
+++ The Millionth Fibonacci Kata.js
@@ -1,28 +1,25 @@
 // https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/javascript
 
 function fib(n) {
-    if(n === 0) {
-        return 0n;
-    } else {
-        if(n === 1 || n === -1) {
-            return 1n;
-        } else {
-            let indexNum = Math.abs(n);
-            let index = 1;
-            let num1 = 0n;
-            let num2 = 1n;
-            let fibNum;
-            while(index < indexNum) {
-                fibNum = num1 + num2;
-                num1 = num2;
-                num2 = fibNum;
-                index += 1;
-            }
-            if(n < 0 && n % 2 === 0) {
-                return -fibNum;
-            } else {
-                return fibNum;
-            }
-        }
+  if (n === 0) {
+    return 0n;
+  } else if (n === 1 || n === -1) {
+    return 1n;
+  } else {
+    const indexNum = Math.abs(n);
+    let num1 = 0n;
+    let num2 = 1n;
+    let fibNum;
+
+    for (let index = 1; index < indexNum; index += 1) {
+      fibNum = num1 + num2;
+      num1 = num2;
+      num2 = fibNum;
     }
+
+    if (n < 0 && n % 2 === 0) {
+      fibNum = -fibNum;
+    }
+    return fibNum;
+  }
 }
